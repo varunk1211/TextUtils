dfca03308adc19876230c47a87627995b0f4b733
varunk1211 <157267521+varunk1211@users.noreply.github.com>
2024-10-31 13:35:44 +0530

    deployment
    this I an text editing
    app

---
+++ precache-manifest.9aef0adafeb219fbbd3ab73ab5e41875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af94641bca778adfa5220173a90be023",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "f758b1d826ab382f090d",
+    "url": "/TextUtils/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5f6cbd984d7989a13b4b",
+    "url": "/TextUtils/static/js/2.59fa9933.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils/static/js/2.59fa9933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cd5cb7402eee1cdf236",
+    "url": "/TextUtils/static/js/3.01dc15d8.chunk.js"
+  },
+  {
+    "revision": "f758b1d826ab382f090d",
+    "url": "/TextUtils/static/js/main.bcafb6be.chunk.js"
+  },
+  {
+    "revision": "a0e0b181d4648abeb69a",
+    "url": "/TextUtils/static/js/runtime-main.2b5e6f64.js"
+  }
+]);
